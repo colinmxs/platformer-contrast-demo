c0bd34d8e5e2925c82e7d7d8154554e136b956cb
Colin Smith <smith.colin00@gmail.com>
2021-02-03 20:50:32 -0700

    el fin... for now. on to bigger and better things

---
+++ contrast_demo/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,36 @@
+﻿namespace ContrastDemo
+{
+    using UnityEngine;
+
+    public class FollowPlayer : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _player;
+        [SerializeField]
+        private bool _followY;
+        [SerializeField]
+        private bool _followX;
+        [SerializeField]
+        private float _smoothing;
+
+        private Vector3 _startingPosition;
+
+        private void Start()
+        {
+            _startingPosition = this.gameObject.transform.position;
+        }
+
+        private void Update()
+        {
+            Vector3 newPosition = 
+                new Vector3(
+                    _followX ? _player.transform.position.x + _startingPosition.x : this.gameObject.transform.position.x,
+                    _followY ? _player.transform.position.y + _startingPosition.y : this.gameObject.transform.position.y,
+                    _startingPosition.z);
+            
+            
+
+            this.gameObject.transform.position = newPosition;
+        }
+    }
+}
